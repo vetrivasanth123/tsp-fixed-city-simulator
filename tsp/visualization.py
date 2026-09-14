96933c721931b441622f9a22d93402e77ee6c5e3
Vasanth kumar <vetrivasanth123@gmail.com>
2026-09-15 00:59:32 +0530

    Update visualization.py

--- tsp/visualization.py
+++ tsp/visualization.py
@@ -143,7 +143,7 @@ def animate_simulation(
             xytext=q1,
             arrowprops={
                 "arrowstyle": "->",
-                "color": "green",
+                "color": "orange",
                 "linewidth": 2,
             },
             zorder=6,
