3a23d4c9619fe76df70076e8b11ba4355687c123
Vasanth kumar <vetrivasanth123@gmail.com>
2026-09-14 23:22:46 +0530

    Update run_flexible_city.py

--- examples/run_flexible_city.py
+++ examples/run_flexible_city.py
@@ -3,7 +3,7 @@ import random
 import sys
 from pathlib import Path
 
-PROJECT_ROOT = Path(**file**).resolve().parents[1]
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(PROJECT_ROOT))
 
 from tsp.city_generator import CityLocationGenerator
