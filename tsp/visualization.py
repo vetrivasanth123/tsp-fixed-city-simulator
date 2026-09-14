a8af83c0c0aa310ed1333bb5294b95520e035254
Vasanth kumar <vetrivasanth123@gmail.com>
2026-09-15 00:50:48 +0530

    Change arrow color from orange to black

--- tsp/visualization.py
+++ tsp/visualization.py
@@ -143,7 +143,7 @@ def animate_simulation(
             xytext=q1,
             arrowprops={
                 "arrowstyle": "->",
-                "color": orange,
+                "color": black,
                 "linewidth": 2,
             },
             zorder=6,
